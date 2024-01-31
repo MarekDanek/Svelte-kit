6b4a6f36fc545ca0a9ca5ec1051e50babeb4cfcb
Marek Danek <MarekDanek05@seznam.cz>
2024-01-31 10:35:29 +0100

    Table

---
+++ src/routes/Tab/Table.svelte
@@ -0,0 +1,73 @@
+<script>
+    import { onMount } from 'svelte';
+      import Bunka from './Bunka.svelte';
+  
+    export let headers;
+    export let rows;
+  
+    const renderFunctions = {
+      name: (value) => `${value}`,
+      level: (value) => `<strong>${value}</strong>`,
+      type: (value) => `<strong>${value}</strong>`,
+      ability: (value) => `<strong>${value}</strong>`,
+    };
+  
+    let minLevel;
+    let maxLevel;
+  
+  
+    let visibleRows = [];
+
+  
+    onMount(() => {
+      minLevel = Math.min(...rows.map(row => row.level));
+      maxLevel = Math.max(...rows.map(row => row.level));
+      visibleRows = rows.slice();
+    });
+  
+  </script>
+  
+  <style>
+    table {
+      border: solid black 2px;
+      border-collapse: collapse;
+      width: 100%;
+    }
+  
+    th, td {
+      border: solid black 1px;
+      padding: 8px;
+      text-align: left;
+    }
+  
+    th {
+      background-color: grey;
+      cursor: pointer;
+    }
+  </style>
+  
+  
+  
+  
+  <table>
+    <thead>
+      <tr>
+        {#each headers as { key, value }}
+          <th>{value}</th>
+        {/each}
+      </tr>
+    </thead>
+    <tbody>
+      {#each rows as row, i (i)}
+        <tr>
+          {#each headers as item}
+                <Bunka>
+                          <!-- 2- tady zase říkám, že tohle je místo "cell" -->
+                          <slot name="cell" cell="{row[item.key]}" />
+            </Bunka>
+          {/each}
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+  
